38b1133003bd28fc9d723b3527f95967ba56c141
ameyallibg <bgameyalli@gmail.com>
2019-07-04 19:10:22 -0500

    Subiendo navbar tope 0px

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, {});
+//   });
 
-  // Or with jQuery
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
 
+  
+$(document).ready(function(){
+  $(window).scroll(function(){
+  if($(window).scrollTop()>500){
+    $('nav').addClass('color');
+  }else{
+    $('nav').removeClass('color');
+  }
+})
+})
