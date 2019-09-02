025bbd3c55df1eb650bf4db3192a09aaee037c2d
ameyallibg <bgameyalli@gmail.com>
2019-09-02 00:51:31 -0500

    Subiendo efectos repo

--- index.js
+++ index.js
@@ -2,6 +2,31 @@
 //     var elems = document.querySelectorAll('.sidenav');
 //     var instances = M.Sidenav.init(elems, {});
 //   });
+$(window).scroll(function() {
+  $('#ame-img').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+400) {
+      $(this).addClass("fadeInUp");
+      $(this).addClass("mostrar");
+    }
+  });
+});
+
+$(window).scroll(function() {
+  $('#about-me').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+400) {
+      $(this).addClass("fadeInUp");
+      $(this).addClass("mostrar");
+    }
+  });
+});
+
+
 
 $(document).ready(function(){
   $('.sidenav').sidenav();
